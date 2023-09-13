f89c462434aef2328a3f73fca2d61f41788583a0
jeroth <jeroth.diggeden@protonmail.com>
2023-09-13 13:04:34 +0800

    Latest

--- main.py
+++ main.py
@@ -19,12 +19,21 @@ if select.startswith("Open"):
             print("Temperatures: \n" + temps)
             number_str = temps.split(", ")
             numbers = [int(num_str) for num_str in number_str]
-            to_do = input("Would you like to 1.Average, 2.Sort or 3.Filter the data?")
+            to_do = input("Would you like to 1.Average or 2.Sort the data?")
 
             if to_do.startswith("1"):
                 avg = sum(numbers) / len(numbers)
                 avg = str(avg)
                 print("The Average Temperature Is: " + avg)
+
+            if to_do.startswith("2"):
+                temps_str = temps.replace(",", "")
+                number_str = temps_str.split()
+                number_lst = [int(num_str) for num_str in number_str]
+                number_lst.sort()
+                number_lst = str(number_lst)
+                print("Here is the sorted data: " + number_lst)
+                select = input("Open, View, Edit, Exit: ")
 
     if open_file.startswith("2"):
         with open("data/wind.txt", "r") as file:
@@ -32,12 +41,45 @@ if select.startswith("Open"):
             print("Wind Speed: \n" + wind)
             number_flt = wind.split(", ")
             numbers = [float(num_flt) for num_flt in number_flt]
-            to_do = input("Would you like to 1.Average, 2.Sort or 3.Filter the data?")
+            to_do = input("Would you like to 1.Average or 2.Sort the data?")
 
             if to_do.startswith("1"):
                 avg = sum(numbers) / len(numbers)
                 avg = str(avg)
                 print("The Average Wind Speed Is: " + avg)
                 select = input("Open, View, Edit, Exit: ")
-                
+
+            if to_do.startswith("2"):
+                wind_str = wind.replace(",", "")
+                number_str = wind_str.split()
+                number_lst = [int(num_str) for num_str in number_str]
+                number_lst.sort()
+                number_lst = str(number_lst)
+                print("Here is the sorted data: " + number_lst)
+                select = input("Open, View, Edit, Exit: ")
+
+    if open_file.startswith("3"):
+        with open("data/rain.txt", "r") as file:
+            rain = file.read()
+            print("Rain(MM): \n" + rain)
+            number_flt = rain.split(", ")
+            numbers = [float(num_flt) for num_flt in number_flt]
+            to_do = input("Would you like to 1.Average or 2.Sort the data?")
+
+            if to_do.startswith("1"):
+                avg = sum(numbers) / len(numbers)
+                avg = str(avg)
+                print("The Average Rainfall in MM is: " + avg)
+                select = input("Open, View, Edit, Exit: ")
+
+            if to_do.startswith("2"):
+                rain_str = rain.replace(",", "")
+                number_str = rain_str.split()
+                number_lst = [int(num_str) for num_str in number_str]
+                number_lst.sort()
+                number_lst = str(number_lst)
+                print("Here is the sorted data: " + number_lst)
+                select = input("Open, View, Edit, Exit: ")
+
+
 
